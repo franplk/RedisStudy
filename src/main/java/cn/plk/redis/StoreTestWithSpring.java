0cfc4db597ad6de504e8f92169032477a945d6fc
franplk <franplk@126.com>
2017-05-09 17:06:22 +0800

    Update Redis ADDING Spring Support

--- src/main/java/cn/plk/redis/StoreTestWithSpring.java
+++ src/main/java/cn/plk/redis/StoreTestWithSpring.java
@@ -5,7 +5,8 @@ import java.util.Set;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,14 +20,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class StoreTestWithSpring extends AbstractJUnit4SpringContextTests {
 
 	@Autowired
-	private StringRedisTemplate redisTemplate;
+	private RedisTemplate<String, Object> redisTemplate;
 	
 	@Test
 	public void test() {
 		Set<String> keys = redisTemplate.keys("*");
+		//redisTemplate.
+		ValueOperations<String, Object> value = redisTemplate.opsForValue();
 		for(String key : keys){
-			System.out.println(key);
-			//redisTemplate.
+			System.out.println(key + "=" + value.get(key));
 		}
 	}
 }
